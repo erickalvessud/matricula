a0d8f43831408585046b2c62638e05770a69c9e1
Erick <erickalvessud@gmail.com>
2016-01-10 16:41:16 -0200

    Alteração de import.

--- src/main/java/br/com/erick/desafioalgaworks4/matricula/converter/AlunoConverter.java
+++ src/main/java/br/com/erick/desafioalgaworks4/matricula/converter/AlunoConverter.java
@@ -4,7 +4,6 @@ import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
-import javax.inject.Inject;
 
 import br.com.erick.desafioalgaworks4.matricula.dao.AlunoDAO;
 import br.com.erick.desafioalgaworks4.matricula.dao.DAO;
