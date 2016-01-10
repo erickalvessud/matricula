20d4e43f9722ccd38914c99f57a8c4494fee4c79
Erick <erickalvessud@gmail.com>
2016-01-10 00:30:33 -0200

    Removi um import.

--- src/main/java/br/com/erick/desafioalgaworks4/matricula/modelo/Matricula.java
+++ src/main/java/br/com/erick/desafioalgaworks4/matricula/modelo/Matricula.java
@@ -2,7 +2,6 @@ package br.com.erick.desafioalgaworks4.matricula.modelo;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
