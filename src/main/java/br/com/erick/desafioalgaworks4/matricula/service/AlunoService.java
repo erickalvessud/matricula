5cb68647150394a4568e49907737a02a70f18a20
Erick <erickalvessud@gmail.com>
2016-01-10 16:43:20 -0200

    Alteração de import e inclusão de comentários.

--- src/main/java/br/com/erick/desafioalgaworks4/matricula/service/AlunoService.java
+++ src/main/java/br/com/erick/desafioalgaworks4/matricula/service/AlunoService.java
@@ -2,10 +2,14 @@ package br.com.erick.desafioalgaworks4.matricula.service;
 
 import javax.inject.Inject;
 
-import br.com.erick.desafioalgaworks4.matricula.dao.AlunoDAO;
 import br.com.erick.desafioalgaworks4.matricula.dao.DAO;
 import br.com.erick.desafioalgaworks4.matricula.modelo.Aluno;
 
+/**
+ * Classe responsavel pela regra de negocio para a entidade {@link Aluno}.
+ * @author Erick Alves
+ *
+ */
 public class AlunoService extends AbstractService<Aluno>{
 	
 	private static final long serialVersionUID = 1L;
