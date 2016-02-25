39d5340d43e581bad1d124538de8ecc63b5f90a0
ealves <ealves@MGDESK0662.magna>
2016-02-25 18:13:00 -0300

    Alteração no utils do JSF

--- src/main/java/br/com/erick/desafioalgaworks4/matricula/util/jsf/FacesUtil.java
+++ src/main/java/br/com/erick/desafioalgaworks4/matricula/util/jsf/FacesUtil.java
@@ -3,17 +3,60 @@ package br.com.erick.desafioalgaworks4.matricula.util.jsf;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
+/**
+ * Classe com metodos comuns para trabalhar com as funcionalidades
+ * disponibilizadas pelo contexto JSF.
+ * 
+ * @author Erick Alves
+ *
+ */
 public class FacesUtil {
-	
+
+	/**
+	 * Adiciona a fila de mensagens do contexto JSF uma mensagem informativa
+	 * para ser exibida para o usuario.
+	 * 
+	 * @param message
+	 *            Mensagem que sera exibida
+	 */
 	public static void addSuccessMessage(String message) {
-		FacesContext.getCurrentInstance().addMessage(null, 
-				new FacesMessage(FacesMessage.SEVERITY_INFO,
-						message, message)); 
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, message, message));
 	}
-	
+
+	/**
+	 * Adiciona a fila de mensagens do contexto JSF uma mensagem de alerta para
+	 * ser exibida para o usuario.
+	 * 
+	 * @param message
+	 *            Mensagem que sera exibida
+	 */
+	public static void addWarnMessage(String message) {
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_WARN, message, message));
+	}
+
+	/**
+	 * Adiciona a fila de mensagens do contexto JSF uma mensagem de erro para
+	 * ser exibida para o usuario.
+	 * 
+	 * @param message
+	 *            Mensagem que sera exibida
+	 */
 	public static void addErrorMessage(String message) {
-		FacesContext.getCurrentInstance().addMessage(null, 
-				new FacesMessage(FacesMessage.SEVERITY_ERROR,
-						message, message)); 
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_ERROR, message, message));
+	}
+
+	/**
+	 * Adiciona a fila de mensagens do contexto JSF uma mensagem fatal para ser
+	 * exibida para o usuario.
+	 * 
+	 * @param message
+	 *            Mensagem que sera exibida
+	 */
+	public static void addFatalMessage(String message) {
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_FATAL, message, message));
 	}
 }
